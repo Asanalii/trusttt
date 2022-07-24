c7badedb4fcf3a83baf109baf92a4ab5dcb3de6f
Asanalii <asanaliiospannn@gmail.com>
2022-07-24 12:07:59 +0600

    New changes2

--- routes/user_cont_route.js
+++ routes/user_cont_route.js
@@ -2,11 +2,10 @@ const express = require('express')
 const UserController = require('./../controllers/user_controller')
 const router = express.Router();
 
-router.get('/',UserController.findAll);
+/*router.get('/',UserController.findAll);
 router.get('/:id',UserController.findOne);
-//router.post('/',UserController.create);
 router.patch('/:id',UserController.update);
-router.delete('/:id',UserController.destroy);
+router.delete('/:id',UserController.destroy);*/
 
 router.post('/login', UserController.login);
 router.post('/register', UserController.register);
